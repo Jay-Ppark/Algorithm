85e6f43e410f697a2180b47ad66fe41438f60681
Jaehyung <godjaehyung@gmail.com>
2021-08-19 02:26:42 +0900

    solve 1920

--- 1920_수찾기.cpp
+++ 1920_수찾기.cpp
@@ -1,46 +1,42 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int arr[100000];
-int findValue(int start_index, int end_index, int find_num)
-{
-	while (start_index <= end_index)
-	{
-		int mid = (start_index + end_index) / 2;
-		int midvalue = arr[mid];
-		if (midvalue < find_num)
-		{
-			start_index = mid + 1;
+int arr[100001];
+int findNum(int N,int t){
+	int start_index=0;
+	int end_index=N;
+	while(start_index<=end_index){
+		int mid_index = (start_index+end_index)/2;
+		int mid_value = arr[mid_index];
+		if(mid_value<t){
+			start_index=mid_index+1;
 		}
-		else if (midvalue > find_num)
-		{
-			end_index = mid - 1;
+		else if(mid_value>t){
+			end_index=mid_index-1;
 		}
-		else
-		{
+		else{
 			return 1;
 		}
 	}
 	return 0;
 }
-int main(void)
-{
+int main(void){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	int N;
-	cin >> N;
-	int answer_num = 0;
-	int num = 0;
-	for (int i = 0; i < N; i++)
-	{
-		cin >> arr[i];
+	cin>>N;
+	for(int i=0;i<N;i++){
+		int tmp;
+		cin>>tmp;
+		arr[i]=tmp;
 	}
-	cin >> answer_num;
-	sort(arr, arr+N);
-	for (int i = 1; i <= answer_num; i++)
-	{
-		cin >> num;
-		cout << findValue(0, N - 1, num) << '\n';
+	sort(arr,arr+N);
+	int M;
+	cin>>M;
+	for(int i=0;i<M;i++){
+		int tmp;
+		cin>>tmp;
+		cout<<findNum(N-1,tmp)<<'\n';
 	}
 	return 0;
 }
