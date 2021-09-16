d004445bd40f7507998696c38c7946b56f23a1fe
Jaehyung <godjaehyung@gmail.com>
2021-09-16 10:26:28 +0900

    solve 0916 algo

--- 1260_DFS와BFS.cpp
+++ 1260_DFS와BFS.cpp
@@ -5,53 +5,53 @@
 using namespace std;
 vector<int> v[1001];
 bool visited[1001];
-void DFS(int s) {
-	if (visited[s]) {
+void init(){
+	for(int i=0;i<1001;i++){
+		visited[i]=false;
+	}
+}
+void DFS(int s){
+	if(visited[s]){
 		return;
 	}
-	cout << s << " ";
-	visited[s] = true;
-	for (int i = 0; i < v[s].size(); i++) {
+	cout<<s<<' ';
+	visited[s]=true;
+	for(int i=0;i<v[s].size();i++){
 		DFS(v[s][i]);
 	}
 }
-void BFS(int s) {
-	visited[s] = true;
+void BFS(int s){
+	visited[s]=true;
 	queue<int> q;
 	q.push(s);
-	while (!q.empty()) {
+	while(!q.empty()){
 		int tmp = q.front();
 		q.pop();
-		cout << tmp << " ";
-		for (int i = 0; i < v[tmp].size(); i++) {
-			if (!visited[v[tmp][i]]) {
+		cout<<tmp<<' ';
+		for(int i = 0;i<v[tmp].size();i++){
+			if(!visited[v[tmp][i]]){
 				q.push(v[tmp][i]);
-				visited[v[tmp][i]] = true;
+				visited[v[tmp][i]]=true;
 			}
 		}
 	}
 }
-void init() {
-	for (int i = 0; i <= 1000; i++) {
-		visited[i] = false;
-	}
-}
-int main(void) {
-	int N, M;
-	int start;
-	cin >> N >> M >> start;
-	for (int i = 0; i < M; i++) {
-		int s, e;
-		cin >> s >> e;
+int main(void){
+	int N,M;
+	int startn;
+	cin>>N>>M>>startn;
+	for(int i=0;i<M;i++){
+		int s,e;
+		cin>>s>>e;
 		v[s].push_back(e);
 		v[e].push_back(s);
 	}
-	for (int i = 1; i <= N; i++) {
-		sort(v[i].begin(), v[i].end());
+	for(int i=1;i<=N;i++){
+		sort(v[i].begin(),v[i].end());
 	}
-	DFS(start);
-	cout << '\n';
+	DFS(startn);
+	cout<<'\n';
 	init();
-	BFS(start);
+	BFS(startn);
 	return 0;
 }
