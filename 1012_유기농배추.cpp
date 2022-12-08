a696a7e78a5021448f3e34f621fb10c4a0e7fff3
Jaehyung <godjaehyung@gmail.com>
2022-12-08 16:05:36 +0900

    study and solve algo problems 221208

--- 1012_유기농배추.cpp
+++ 1012_유기농배추.cpp
@@ -1,60 +1,52 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-vector<int> result;
+int N,M,K;
+int veg[50][50];
 bool visited[50][50];
-int grd[50][50];
-int M, N, K;
-int cnt;
-int dy[4] = { -1,1,0,0 };
-int dx[4] = { 0,0,-1,1 };
-void init() {
-	for (int i = 0; i < 50; i++) {
-		for (int j = 0; j < 50; j++) {
-			visited[i][j] = false;
-			grd[i][j] = 0;
-		}
-	}
-}
-void DFS(int y, int x) {
-	if (visited[y][x]) {
-		return;
-	}
-	visited[y][x] = true;
-	for (int i = 0; i < 4; i++) {
-		int tmpy = y + dy[i];
-		int tmpx = x + dx[i];
-		if (tmpy >= 0 && tmpy < M && tmpx >= 0 && tmpx < N && !visited[tmpy][tmpx]) {
-			if (grd[tmpy][tmpx] == 1) {
-				DFS(tmpy, tmpx);
+int dy[4]={-1,0,1,0};
+int dx[4]={0,-1,0,1};
+int cnt=0;
+void DFS(int y,int x){
+	visited[y][x]=true;
+	for(int i=0;i<4;i++){
+		int ny=y+dy[i];
+		int nx=x+dx[i];
+		if(ny>=0&&ny<N&&nx>=0&&nx<M){
+			if(!visited[ny][nx]&&veg[ny][nx]==1){
+				DFS(ny,nx);
 			}
 		}
 	}
 }
-int main(void) {
-	int testcase;
-	cin >> testcase;
-	for (int i = 0; i < testcase; i++) {
-		init();
-		cin >> M >> N >> K;
-		for (int i = 0; i < K; i++) {
-			int tmpy, tmpx;
-			cin >> tmpy >> tmpx;
-			grd[tmpy][tmpx] = 1;
+void init(){
+	for(int i=0;i<50;i++){
+		for(int j=0;j<50;j++){
+			visited[i][j]=false;
+			veg[i][j]=0;
 		}
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j] && grd[i][j]==1 ) {
-					DFS(i, j);
+	}
+	cnt=0;
+}
+int main(void){
+	int testcase;
+	cin>>testcase;
+	for(int t=0;t<testcase;t++){
+		cin>>M>>N>>K;
+		for(int i=0;i<K;i++){
+			int tmpx,tmpy;
+			cin>>tmpx>>tmpy;
+			veg[tmpy][tmpx]=1;
+		}
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				if(!visited[i][j]&&veg[i][j]==1){
+					DFS(i,j);
 					cnt++;
 				}
 			}
 		}
-		result.push_back(cnt);
-		cnt = 0;
-	}
-	for (int j = 0; j < result.size(); j++) {
-		cout << result[j] << '\n';
+		cout<<cnt<<'\n';
+		init();
 	}
 	return 0;
 }
