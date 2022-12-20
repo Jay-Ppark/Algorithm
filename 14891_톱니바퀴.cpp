2145c349f6fcd83ae245ae85650d010f490a6fb6
Jaehyung <godjaehyung@gmail.com>
2022-12-21 00:28:07 +0900

    study and solve algo problems 221221

--- 14891_톱니바퀴.cpp
+++ 14891_톱니바퀴.cpp
@@ -1,280 +1,127 @@
 #include<iostream>
 #include<string>
-#include<cmath>
 using namespace std;
 string gear[4];
-int gindex[4];
-void keepmove(int g, int d) {
-	if (g == 0) {
-		if (d == 1) {
-			if (gear[0][(gindex[0] + 2) % 8] != gear[1][(gindex[1] + 6) % 8]) {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-					else {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-					}
-				}
-				else {
-					gindex[0] = (gindex[0] + 7) % 8;
-					gindex[1] = (gindex[1] + 1) % 8;
-				}
-			}
-			else {
-				gindex[0] = (gindex[0] + 7) % 8;
-			}
-		}
-		else {
-			if (gear[0][(gindex[0] + 2) % 8] != gear[1][(gindex[1] + 6) % 8]) {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-					else {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-					}
-				}
-				else {
-					gindex[0] = (gindex[0] + 1) % 8;
-					gindex[1] = (gindex[1] + 7) % 8;
-				}
-			}
-			else {
-				gindex[0] = (gindex[0] + 1) % 8;
-			}
+void movegear(int g,int d){
+	string tmp=gear[g];
+	if(d==1){
+		for(int i=0;i<8;i++){
+			gear[g][i]=tmp[(i+7)%8];
 		}
 	}
-	else if (g == 1) {
-		if (d == 1) {
-			if (gear[0][(gindex[0] + 2) % 8] != gear[1][(gindex[1] + 6) % 8]) {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-					else {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-					}
-				}
-				else {
-					gindex[0] = (gindex[0] + 1) % 8;
-					gindex[1] = (gindex[1] + 7) % 8;
-				}
-			}
-			else {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-					}
-				}
-				else {
-					gindex[1] = (gindex[1] + 7) % 8;
-				}
-			}
-		}
-		else {
-			if (gear[0][(gindex[0] + 2) % 8] != gear[1][(gindex[1] + 6) % 8]) {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-					else {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-					}
-				}
-				else {
-					gindex[0] = (gindex[0] + 7) % 8;
-					gindex[1] = (gindex[1] + 1) % 8;
-				}
-			}
-			else {
-				if (gear[1][(gindex[1] + 2) % 8] != gear[2][(gindex[2] + 6) % 8]) {
-					if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-					}
-				}
-				else {
-					gindex[1] = (gindex[1] + 1) % 8;
-				}
-			}
-		}
-	}
-	else if (g == 2) {
-		if (d == 1) {
-			if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 7) % 8;
-					gindex[3] = (gindex[3] + 1) % 8;
-				}
-			}
-			else {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 7) % 8;
-				}
-			}
-		}
-		else {
-			if (gear[2][(gindex[2] + 2) % 8] != gear[3][(gindex[3] + 6) % 8]) {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 1) % 8;
-					gindex[3] = (gindex[3] + 7) % 8;
-				}
-			}
-			else {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 1) % 8;
-				}
-			}
-		}
-	}
-	else {
-		if (d == 1) {
-			if (gear[3][(gindex[3] + 6) % 8] != gear[2][(gindex[2] + 2) % 8]) {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 1) % 8;
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 7) % 8;
-						gindex[2] = (gindex[2] + 1) % 8;
-						gindex[3] = (gindex[3] + 7) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 1) % 8;
-					gindex[3] = (gindex[3] + 7) % 8;
-				}
-			}
-			else {
-				gindex[3] = (gindex[3] + 7) % 8;
-			}
-		}
-		else {
-			if (gear[3][(gindex[3] + 6) % 8] != gear[2][(gindex[2] + 2) % 8]) {
-				if (gear[2][(gindex[2] + 6) % 8] != gear[1][(gindex[1] + 2) % 8]) {
-					if (gear[1][(gindex[1] + 6) % 8] != gear[0][(gindex[0] + 2) % 8]) {
-						gindex[0] = (gindex[0] + 7) % 8;
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-					else {
-						gindex[1] = (gindex[1] + 1) % 8;
-						gindex[2] = (gindex[2] + 7) % 8;
-						gindex[3] = (gindex[3] + 1) % 8;
-					}
-				}
-				else {
-					gindex[2] = (gindex[2] + 7) % 8;
-					gindex[3] = (gindex[3] + 1) % 8;
-				}
-			}
-			else {
-				gindex[3] = (gindex[3] + 1) % 8;
-			}
+	else{
+		for(int i=0;i<8;i++){
+			gear[g][i]=tmp[(i+1)%8];
 		}
 	}
 }
-int main(void) {
-	for (int i = 0; i < 4; i++) {
-		cin >> gear[i];
-	}
-	int testcase;
-	cin >> testcase;
-	for (int i = 0; i < testcase; i++) {
-		int gearnum, dir;
-		cin >> gearnum >> dir;
-		keepmove(gearnum-1, dir);
-	}
-	int result = 0;
-	for (int i = 0; i < 4; i++) {
-		if (gear[i][gindex[i]] == '1') {
-			result = result + (int)pow(2, i);
+void checkmove(int g,int d){
+	if(g==0){
+		if(gear[0][2]!=gear[1][6]){
+			if(gear[1][2]!=gear[2][6]){
+				if(gear[2][2]!=gear[3][6]){
+					movegear(0,d);
+					movegear(1,d*-1);
+					movegear(2,d);
+					movegear(3,d*-1);
+				}
+				else{
+					movegear(0,d);
+					movegear(1,d*-1);
+					movegear(2,d);
+				}
+			}
+			else{
+				movegear(0,d);
+				movegear(1,d*-1);
+			}
+		}
+		else{
+			movegear(0,d);
 		}
 	}
-	cout << result;
+	else if(g==1){
+		if(gear[1][6]!=gear[0][2]){
+			movegear(0,d*-1);
+		}
+		if(gear[1][2]!=gear[2][6]){
+			if(gear[2][2]!=gear[3][6]){
+				movegear(1,d);
+				movegear(2,d*-1);
+				movegear(3,d);
+			}
+			else{
+				movegear(1,d);
+				movegear(2,d*-1);
+			}
+		}
+		else{
+			movegear(1,d);
+		}
+	}
+	else if(g==2){
+		if(gear[2][2]!=gear[3][6]){
+			movegear(3,d*-1);
+		}
+		if(gear[2][6]!=gear[1][2]){
+			if(gear[1][6]!=gear[0][2]){
+				movegear(2,d);
+				movegear(1,d*-1);
+				movegear(0,d);
+			}
+			else{
+				movegear(2,d);
+				movegear(1,d*-1);
+			}
+		}
+		else{
+			movegear(2,d);
+		}
+	}
+	else{
+		if(gear[3][6]!=gear[2][2]){
+			if(gear[2][6]!=gear[1][2]){
+				if(gear[1][6]!=gear[0][2]){
+					movegear(3,d);
+					movegear(2,d*-1);
+					movegear(1,d);
+					movegear(0,d*-1);
+				}
+				else{
+					movegear(3,d);
+					movegear(2,d*-1);
+					movegear(1,d);
+				}
+			}
+			else{
+				movegear(3,d);
+				movegear(2,d*-1);
+			}
+		}
+		else{
+			movegear(3,d);
+		}
+	}
+}
+int main(void){
+	for(int i=0;i<4;i++){
+		cin>>gear[i];
+	}
+	int K;
+	cin>>K;
+	for(int i=0;i<K;i++){
+		int tmpg, tmpd;
+		cin>>tmpg>>tmpd;
+		checkmove(tmpg-1,tmpd);
+	}
+	int answer=0;
+	int index=1;
+	for(int i=0;i<4;i++){
+		if(gear[i][0]=='1'){
+			answer=answer+index;
+		}
+		index=index*2;
+	}
+	cout<<answer;
 	return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
+string gear[3];
+void movegear(int g,int d){
+	if(d==1){
+		//char tmp=gear[g][7];
+		for(int i=1;i<8;i++){
+			char tmp=gear[g][i-1];
+			cout<<tmp;
+			gear[g][i]=tmp;
+		}
+		//gear[g][0]=tmp;
+	}
+	else{
+		char tmp=gear[g][0];
+		for(int i=0;i<7;i++){
+			gear[g][i]=gear[g][i+1];
+		}
+		//gear[g][7]=tmp;
+	}
+}
 int main(void){
-    string a="abc";
-    string b="efg";
-    cout<<(a>b);
+    cin>>gear[0];
+    movegear(0,1);
+    cout<<gear[0]<<'\n';
+    //movegear(0,-1);
+    //cout<<gear[0];
     return 0;
 }
